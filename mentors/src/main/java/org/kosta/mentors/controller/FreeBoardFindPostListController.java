742afe13aadb456f82f4f8bf1bc8b04185f4dc6c
HunjinKim8830 <issachunjin@gmail.com>
2022-11-02 17:51:02 +0900

    FreeBoardFindPostListController-자유게시판 리스트 출력 freeboard/list.jsp by khj

---
+++ mentors/src/main/java/org/kosta/mentors/controller/FreeBoardFindPostListController.java
@@ -0,0 +1,18 @@
+package org.kosta.mentors.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kosta.mentors.model.FreeBoardDAO;
+
+
+
+public class FreeBoardFindPostListController implements Controller {
+	@Override
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setAttribute("list", FreeBoardDAO.getInstance().findPostList());
+		request.setAttribute("url", "freeboard/list.jsp");
+		return "layout.jsp";
+	}
+
+}
