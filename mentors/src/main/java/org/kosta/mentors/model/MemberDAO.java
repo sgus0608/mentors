a58a0758f3c5f8d74ed8dbf70feac9bca6d41590
Donghun13 <kddonghoon@naver.com>
2022-11-02 12:40:05 +0900

    MemberVO 및 MemberDAO 수정하여 커밋

--- mentors/src/main/java/org/kosta/mentors/model/MemberDAO.java
+++ mentors/src/main/java/org/kosta/mentors/model/MemberDAO.java
@@ -10,26 +10,29 @@ import javax.sql.DataSource;
 public class MemberDAO {
 	private static MemberDAO instance= new MemberDAO();
 	private DataSource dataSource;
+	
 	private MemberDAO() {
 		dataSource = DataSourceManager.getInstance().getDataSource();	
 	}
+	
 	public static MemberDAO getInstance() {
 		return instance;
 	}
+	
 	public void closeAll(PreparedStatement pstmt,Connection con) throws SQLException {
 		if(pstmt!=null)
 			pstmt.close();
 		if(con!=null)
 			con.close(); // 컨넥션을 DBCP(DataSource)로 반납한다 
 	}
+	
 	public void closeAll(ResultSet rs,PreparedStatement pstmt,Connection con) throws SQLException {
 		if(rs!=null)
 			rs.close();
-		if(pstmt!=null)
-			pstmt.close();
-		if(con!=null)
-			con.close();// 컨넥션을 DBCP(DataSource)로 반납한다 
+		closeAll(pstmt, con); // 컨넥션을 DBCP(DataSource)로 반납한다 
 	}
+	
+	// 로그인 메서드
 	public MemberVO login(String id, String password) throws SQLException {
 		MemberVO memberVO = null;
 		Connection con = null;
@@ -37,13 +40,15 @@ public class MemberDAO {
 		ResultSet rs = null;
 		try {
 			con = dataSource.getConnection();
-			String sql = "select nick_name from mentors_member where id=? and password=?";
-			pstmt=con.prepareStatement(sql);
+			StringBuilder sql = new StringBuilder();
+			sql.append("SELECT nick_name, email, address, interest, TO_CHAR(signup_date, 'YYYY-MM-DD') as signup_date, member_type ");
+			sql.append("FROM mentors_member WHERE id=? and password=?");
+			pstmt=con.prepareStatement(sql.toString());
 			pstmt.setString(1, id);
 			pstmt.setString(2, password);
 			rs=pstmt.executeQuery();
 			if(rs.next()) {
-				memberVO=new MemberVO(id, password, rs.getString(1));
+				memberVO = new MemberVO(id, password, rs.getString("nick_name"), rs.getString("email"), rs.getString("address"), rs.getString("interest"), rs.getString("signup_date"), rs.getString("member_type"));
 			}
 		} finally {
 			closeAll(rs, pstmt, con);
--- mentors/src/main/java/org/kosta/mentors/model/MemberVO.java
+++ mentors/src/main/java/org/kosta/mentors/model/MemberVO.java
@@ -2,8 +2,8 @@ package org.kosta.mentors.model;
 
 import java.io.Serializable;
 
-public class MemberVO implements Serializable{
-	
+public class MemberVO implements Serializable {
+
 	private static final long serialVersionUID = 6079982092097564268L;
 	private String id;
 	private String password;
@@ -11,14 +11,15 @@ public class MemberVO implements Serializable{
 	private String email;
 	private String address;
 	private String interest;
-	private String signupDate;
-	private String MemberType;
+	private String signUpDate;
+	private String memberType;
+
 	public MemberVO() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
+
 	public MemberVO(String id, String password, String nickName, String email, String address, String interest,
-			String signupDate, String memberType) {
+			String signUpDate, String memberType) {
 		super();
 		this.id = id;
 		this.password = password;
@@ -26,71 +27,79 @@ public class MemberVO implements Serializable{
 		this.email = email;
 		this.address = address;
 		this.interest = interest;
-		this.signupDate = signupDate;
-		MemberType = memberType;
+		this.signUpDate = signUpDate;
+		this.memberType = memberType;
 	}
-	
-	public MemberVO(String id, String password, String nickName) {
-		super();
-		this.id = id;
-		this.password = password;
-		this.nickName = nickName;
-	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getNickName() {
 		return nickName;
 	}
+
 	public void setNickName(String nickName) {
 		this.nickName = nickName;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getInterest() {
 		return interest;
 	}
+
 	public void setInterest(String interest) {
 		this.interest = interest;
 	}
-	public String getSignupDate() {
-		return signupDate;
+
+	public String getSignUpDate() {
+		return signUpDate;
 	}
-	public void setSignupDate(String signupDate) {
-		this.signupDate = signupDate;
+
+	public void setSignUpDate(String signUpDate) {
+		this.signUpDate = signUpDate;
 	}
+
 	public String getMemberType() {
-		return MemberType;
+		return memberType;
 	}
+
 	public void setMemberType(String memberType) {
-		MemberType = memberType;
+		this.memberType = memberType;
 	}
+
 	@Override
 	public String toString() {
 		return "MemberVO [id=" + id + ", password=" + password + ", nickName=" + nickName + ", email=" + email
-				+ ", address=" + address + ", interest=" + interest + ", signupDate=" + signupDate + ", MemberType="
-				+ MemberType + "]";
+				+ ", address=" + address + ", interest=" + interest + ", signUpDate=" + signUpDate + ", memberType="
+				+ memberType + "]";
 	}
-	
-	
-	
+
 }
