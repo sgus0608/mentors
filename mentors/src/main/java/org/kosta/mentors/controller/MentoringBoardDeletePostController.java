82775f8e63e6b72bdc94ad8cf3168c06de461480
Donghun13 <kddonghoon@naver.com>
2022-11-04 12:09:32 +0900

    멘토링 게시판 삭제 기능 완료

--- mentors/src/main/java/org/kosta/mentors/controller/MentoringBoardDeletePostController.java
+++ mentors/src/main/java/org/kosta/mentors/controller/MentoringBoardDeletePostController.java
@@ -3,11 +3,15 @@ package org.kosta.mentors.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.kosta.mentors.model.MentoringBoardDAO;
+
 public class MentoringBoardDeletePostController implements Controller {
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		return null;
+		long postNo = Long.parseLong(request.getParameter("postNo"));
+		MentoringBoardDAO.getInstance().deletePost(postNo);
+		return "redirect:MentoringBoardFindPostListController.do";
 	}
 
 }
