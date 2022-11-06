b63b98c2346965e5a18709d229fd37e68a0b2171
HunjinKim8830 <issachunjin@gmail.com>
2022-11-06 11:31:58 +0900

    조회수 증가 by khj

--- mentors/src/main/java/org/kosta/mentors/controller/FreeBoardPostDetailController.java
+++ mentors/src/main/java/org/kosta/mentors/controller/FreeBoardPostDetailController.java
@@ -1,7 +1,10 @@
 package org.kosta.mentors.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.kosta.mentors.model.FreeBoardDAO;
 import org.kosta.mentors.model.PostVO;
@@ -9,7 +12,15 @@ import org.kosta.mentors.model.PostVO;
 public class FreeBoardPostDetailController implements Controller {
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		PostVO postVO=FreeBoardDAO.getInstance().postDetailByNo(Long.parseLong(request.getParameter("postNo")));
+		long postNo=Long.parseLong(request.getParameter("postNo"));
+		HttpSession session=request.getSession(false);
+		@SuppressWarnings("unchecked")
+		ArrayList<Long> list=(ArrayList<Long>) session.getAttribute("freeboard");
+		if(list.contains(postNo)==false) {
+			FreeBoardDAO.getInstance().updateHits(postNo);
+			list.add(postNo);
+		}
+		PostVO postVO=FreeBoardDAO.getInstance().postDetailByNo(postNo);
 		request.setAttribute("postVO", postVO);
 		request.setAttribute("url", "freeBoard/freeboard-post-detail.jsp");
 		return "layout.jsp";
