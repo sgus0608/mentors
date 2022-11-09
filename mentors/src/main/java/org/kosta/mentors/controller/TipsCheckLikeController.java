8fc49c7d9c906be64d4e763813272694bc3ea6ad
ryueunjin <ejryu109@naver.com>
2022-11-09 10:06:02 +0900

    tipsboard 좋아요 기능 완료

--- mentors/src/main/java/org/kosta/mentors/controller/TipsCheckLikeController.java
+++ mentors/src/main/java/org/kosta/mentors/controller/TipsCheckLikeController.java
@@ -4,6 +4,8 @@ package org.kosta.mentors.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
 import org.kosta.mentors.model.MemberVO;
 import org.kosta.mentors.model.TipsBoardDAO;
 
@@ -11,20 +13,27 @@ public class TipsCheckLikeController implements Controller {
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		long postNo=Long.parseLong(request.getParameter("postNo"));
 		HttpSession session=request.getSession(false);
 		MemberVO memberVO=(MemberVO) session.getAttribute("mvo");//MemberVO 객체를 담고 있는 정보
 		String id = memberVO.getId();
-		long postNo=Long.parseLong(request.getParameter("postNo"));
+		
 		boolean result=TipsBoardDAO.getInstance().checkLike(postNo,id);
 		String message=null;
-		if(result==true) {
-			message="fail";
+		if(result) {
 			TipsBoardDAO.getInstance().deleteLike(id, postNo);
+			message="fail";
 		}else {
-			message="ok";
 			TipsBoardDAO.getInstance().insertLike(id, postNo);
+			message="ok";
 		}
-		request.setAttribute("responsebody", message);//AjaxViewServlet이 클라이언트에게 응답하도록 저장 
+		
+		long countLike=TipsBoardDAO.getInstance().getTotalLikeCount(postNo);
+		JSONObject json=new JSONObject();
+		json.put("message", message);
+		json.put("countLike", countLike);
+		
+		request.setAttribute("responsebody", json);//AjaxViewServlet이 클라이언트에게 응답하도록 저장 
 		return "AjaxView";
 	}
 
