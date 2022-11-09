0f732b046143b20e2c355aed98dd8b7467b3a18a
ParkJinSoo189 <xnqlwlrh@naver.com>
2022-11-09 12:05:11 +0900

    글수정기능 최종 완료

--- mentors/src/main/java/org/kosta/mentors/controller/QnABoardUpdatePostFormController.java
+++ mentors/src/main/java/org/kosta/mentors/controller/QnABoardUpdatePostFormController.java
@@ -1,5 +1,7 @@
 package org.kosta.mentors.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,6 +15,13 @@ public class QnABoardUpdatePostFormController implements Controller {
 		long postNo=Long.parseLong(request.getParameter("postNo"));
 		QnABoardDAO qnaBoardDAO=QnABoardDAO.getInstance();
 		QnAPostVO qnaPostVO=qnaBoardDAO.postDetailByNo(postNo);
+		
+		ArrayList<String> categoryList=new ArrayList<>();
+		categoryList.add("프로그래밍");
+		categoryList.add("취업");
+		categoryList.add("자격증");
+		
+		request.setAttribute("categoryList", categoryList);
 		request.setAttribute("qnaPostVO", qnaPostVO);
 		request.setAttribute("url", "qnaBoard/qnaboard-update-form.jsp");
 		return "layout.jsp";
--- mentors/src/main/webapp/qnaBoard/qnaboard-update-form.jsp
+++ mentors/src/main/webapp/qnaBoard/qnaboard-update-form.jsp
@@ -8,9 +8,16 @@
 				<input type="hidden" name="postNo" value="${qnaPostVO.postNo}">
 				<select name="category" required="required">
 					<option value="">--카테고리--</option>
-					<option value="프로그래밍">프로그래밍</option>
-					<option value="취업">취업</option>
-					<option value="자격증">자격증</option>
+				  <c:forEach items="${categoryList}" var="cList">
+					<c:choose>
+					  <c:when test="${cList==qnaPostVO.category}">
+					    <option value="${cList}" selected="selected">${cList}</option>
+					  </c:when>
+					  <c:otherwise>
+					    <option value="${cList}">${cList}</option>
+					  </c:otherwise>
+					</c:choose>
+				  </c:forEach>
 				</select>
 				<input type="text" name="title" value="${qnaPostVO.title}" required="required">
 				</td>
